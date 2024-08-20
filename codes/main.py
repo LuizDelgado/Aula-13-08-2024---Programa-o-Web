8591e98c118932a32696e384d562caf73b758005
Luiz Delgado <107853963+LuizDelgado@users.noreply.github.com>
2024-08-20 20:24:34 +0000

    Pending changes exported from your codespace

---
+++ codes/main.py
@@ -0,0 +1,48 @@
+########## Regras de nome de variável ##########
+
+CONSTANTE = 0 #Letras maiusculas indicam constante 
+akljdfhaskljdsh = 10 #Variável int
+sopa_de_batata = "top" #Variável str
+########## O python não aceita nomes com caracteres especiais diferente de _ ##########
+
+########## Criem uma variavel do tipo int e chame ela no print ##########
+x = 10
+print(x)
+
+########## Criem uma variavel do tipo float e chame ela no print ##########
+y = 11.5
+# print(y)
+
+########## Criem uma variavel do tipo str e chame ela no print ##########
+nome = "Luizinho"
+# print(nome)
+
+########## Operações com as variaveis ##########
+print(x + y) #Operação de soma
+print(x - y) #Operação de subtração
+print(x/y)   #Operação de divisão
+print(x*y)   #Operação de multiplicação
+print(x**y)  #Operação exponencial
+
+########## Funções de conversão de variável ##########
+x = float(x) #Função de conversão do float
+numero_string = '847364287463'
+numero_string = int(numero_string) #Função de conversão para inteiro
+x = str(x)
+
+########## Variável Booleana ##########
+booleana = False
+
+
+num = 0
+def exercício():
+   num = 1
+   print(num)
+
+exercício()
+
+
+print(num)
+
+
+
